58f1e16757f18421040deac9e4472799bee4426e
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2009-03-20 20:14:04 +0000

    Don't add history to the description.

--- pleiades.json/trunk/setup.py
+++ pleiades.json/trunk/setup.py
@@ -6,9 +6,7 @@ version = '0.1'
 setup(name='pleiades.json',
       version=version,
       description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      long_description=open("README.txt").read(),
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
